d13abec803e57465df22cc2a7290470755b80804
Max <maximumpwnguin@gmail.com>
2018-07-19 07:33:06 -0700

    Add (unfinished) priority scheduling

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -2,7 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
-
+#include <iterator>
 
 using namespace epoxy::scheduler;
 
@@ -27,6 +27,24 @@ void SynchronousScheduler::addCommand(Command *cmd) {
     commands.push_back(cmd);
 }
 
+void SynchronousScheduler::runCommand(Command* cmd) {
+    switch (cmd->getCommandState()) {
+        case CommandState::UNINITIALIZED:
+            cmd->setCommandState(CommandState::RUNNING);
+            cmd->initialize();
+        break;
+        case CommandState::RUNNING:
+            cmd->update(dt);
+            if (cmd->shouldTerminate()) {
+                cmd->terminate();
+                cmd->setCommandState(CommandState::STOPPED);
+            }
+        break;
+        default:
+        break;
+    }
+}
+
 void SynchronousScheduler::update(int dt) {
     for (auto cmd: commands) {
         switch (cmd->getCommandState()) {
@@ -49,4 +67,59 @@ void SynchronousScheduler::update(int dt) {
         return cmd->getCommandState() == CommandState::STOPPED;
     }), commands.end());
 
+}
+
+CommandQueue::CommandQueue() {
+
+}
+
+CommandQueue::~CommandQueue() {
+    while (first != nullptr) {
+        auto* tmp = first;
+        first = tmp->next;
+        delete tmp;
+    }
+}
+
+void CommandQueue::pushFront(Command* cmd) {
+    Node* second = first;
+    first = new Node { nullptr, second, cmd };
+    if (second == nullptr) {
+        last = first;
+    } else {
+        second->prev = first;
+    }
+}
+
+void CommandQueue::pushBack(Command* cmd) {
+    auto* penult = last;
+    last = new Node { penult, nullptr, cmd };
+    if (penult == nullptr) {
+        first = last;
+    } else {
+        penult->next = last;
+    }
+}
+
+Command* CommandQueue::popFront() {
+    if (first == nullptr) {
+        return nullptr;
+    }
+    auto* tmp = first;
+    first = tmp->next;
+    auto* out = tmp->command;
+    delete tmp;
+    return out;
+}
+
+Command* CommandQueue::Node::remove() {
+    if (prev != nullptr) {
+        prev->next = next;
+    }
+    if (next != nullptr) {
+        next->prev = prev;
+    }
+    auto* out = command;
+    delete this;
+    return out;
 }
--- src/scheduler.hpp
+++ src/scheduler.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iterator>
+#include <list>
 #include <vector>
 
 namespace epoxy {
@@ -8,11 +10,28 @@ namespace epoxy {
             UNINITIALIZED, RUNNING, STOPPED
         };
 
+        struct CommandSchedulingParam {
+            /**
+             * Minimum amount of time that must pass before next update.
+             */
+            int minTime = -1;
+            /**
+             * Maximum amount of time that can pass after last update.
+             */
+            int repTime = -1;
+        };
+
+        class Scheduler;
+
         class Command {
             CommandState state;
+            unsigned long lastRun;
         public:
             Command();
             virtual ~Command() {};
+
+            const CommandSchedulingParam scheduling;
+
             virtual void initialize() { };
             virtual void update(int dt) { };
             virtual bool shouldTerminate() { return false; };
@@ -29,8 +48,29 @@ namespace epoxy {
             virtual void update(int dt) = 0;
         };
 
+
+        class CommandQueue {
+        public:
+            struct Node {
+                Node* prev;
+                Node* next;
+                Command* command;
+                Command* remove();
+            };
+
+            CommandQueue();
+            ~CommandQueue(); 
+            void pushFront(Command* cmd);
+            void pushBack(Command* cmd);
+            Command* popFront();
+            Node* first;
+            Node* last;
+        };
+
         class SynchronousScheduler : public Scheduler {
-            std::vector<Command*> commands;
+            CommandQueue commands;
+            std::list<Command*> priorityCommands;
+            void runCommand(Command* cmd);
         public:
             SynchronousScheduler();
             void addCommand(Command *cmd) override;
